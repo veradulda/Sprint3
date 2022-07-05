303d2460e0544847f694a2dd9e1ab6d676a00eca
veraDulda <99422897+veraDulda@users.noreply.github.com>
2022-07-05 12:33:08 +0300

    US2 after Jira impliment

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
-        tags = "@wip",
+        tags = "",
         plugin = {
                 "html:target/cucumber-report.html",
                 "json:target/cucumber.json"
